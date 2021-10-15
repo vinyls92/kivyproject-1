9a326d12232bd3370f41ae1478c8f094fe2100d2
song <marco02@naver.com>
2021-10-15 14:16:57 +0900

    First commit at home

---
+++ main.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+
+class HomeScreen(Screen):
+    pass
+
+GUI = Builder.load_file("main.kv")
+class MainApp(App):
+    def build(self):
+        return GUI
+
+MainApp().run()
