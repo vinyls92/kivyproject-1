41045788822cea8f1eed2078b29da54a522c1284
user <user@user.com>
2021-10-15 19:05:24 +0900

    2nd commit at office

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ from kivy.uix.screenmanager import Screen
 class HomeScreen(Screen):
     pass
 
+class SettingsScreen(Screen):
+    pass
+
 GUI = Builder.load_file("main.kv")
 class MainApp(App):
     def build(self):
         return GUI
+
+    def change_screen(self, screen_name):
+        # Get the screen manager from the kv file
+        screen_manager = self.root.ids['screen_manager']
+        screen_manager.transition
+        screen_manager.current = screen_name
+        # screen_manager = self.root.ids
 
 MainApp().run()
