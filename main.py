c1007f2cd7cb32837f451dc824b68e758a0aa86b
user <user@user.com>
2021-10-25 19:32:15 +0900

    3rd commit at office

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
+from kivy.uix.button import ButtonBehavior
+from kivy.uix.image import Image
 
 class HomeScreen(Screen):
+    pass
+
+class ImageButton(ButtonBehavior, Image):
     pass
 
 class SettingsScreen(Screen):
